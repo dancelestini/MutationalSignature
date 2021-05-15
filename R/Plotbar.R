e5522c22bb97b4ea76d0d198d80c42e614ddaf0b
dancelestini <71639864+dancelestini@users.noreply.github.com>
2021-05-15 11:15:45 +0200

    Add files via upload

--- R/Plotbar.R
+++ R/Plotbar.R
@@ -1,3 +1,11 @@
+#' Create mutational signature from a cosmic dataset
+#'
+#' This function will create mutational signature from an input dataset. See each function description
+#' for more details.
+#'
+#' @param total Path to the output of Signature_algoV2 function.
+#' @return Mutational signature using ggplot2.
+#' @export
 Plotbar = function(total){
   ggplot(total,aes(x=codons,y=pourcentage, fill = to))+
     geom_bar(stat = "identity", size = 0.5, width = 0.8)+
